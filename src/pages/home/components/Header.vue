cc94c5fe097f9723fe4a8008e870e0005959fcd5
rico <ricocc@qq.com>
2021-07-07 19:50:25 +0800

    full header

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="header">
-    <div class="header-left">首页</div>
-    <div class="header-input">输入</div>
-    <div class="header-right">城市</div>
+    <div class="header-left"><div class="iconfont back-icon">&#xe601;</div></div>
+    <div class="header-input">
+      <span class="iconfont">&#xe603;</span>
+      输入城市/景点/主题</div>
+    <div class="header-right">城市
+      <span class="iconfont arrow-icon">&#xe600;</span>
+    </div>
   </div>
 </template>
 <script>
@@ -11,18 +15,37 @@ export default {
 }
 </script>
 <style lang="stylus" scoped>
+@import '~styles/varibles.styl'
 .header
-  display: flex
-  height: .86rem
-  background:#00dc40
+  display flex
+  height .86rem
+  background $bgColor
   color #fff
+  align-items center
+  justify-content space-around
+  text-align center
+  margin 0 auto
   .header-left
-    width: .64rem
-    float:left
+    width .64rem
+    margin 0 .12rem
+    .back-icon
+      text-align center
+      font-size .4rem
   .header-input
-    flex: 1
+    flex 1
+    align-items cente
+    height .64rem
+    line-height .64rem
+    background #fff
+    text-align left
+    margin 0 .12rem
+    padding-left .2rem
+    border-radius .1rem
+    color #aaa
   .header-right
-    width:1.24rem
-    float:right
-    text-align:center
+    width 1.24rem
+    text-align center
+    .arrow-icon
+      font-size 0.32rem
+      margin-left -0.05rem
 </style>
