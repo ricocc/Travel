2ccd27219abca6259d710adc6fccbb376f0891c0
rico <ricocc@qq.com>
2021-07-21 18:33:20 +0800

    7-7 article

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -83,33 +83,35 @@ export default {
   .icons >>> .swiper-container
     height 0
     padding-bottom 50%
-  .icon
-    position relative
-    overflow hidden
-    float left
-    width 25%
-    height 0
-    padding-bottom 25%
-    .icon-img
-      position absolute
-      top 0
-      left 0
-      right 0
-      bottom .44rem
-      box-sizing border-box
-      padding .1rem
-      img
-        display block
-        margin 0 auto
-        width 100%
-    .icon-info
-      position absolute
-      left 0
-      right 0
-      bottom 0
-      height .40rem
-      line-height .40rem
-      text-align center
-      color $darkTextColor
-      ellipsis()
+  .icons
+    margin-top .1rem
+    .icon
+      position relative
+      overflow hidden
+      float left
+      width 25%
+      height 0
+      padding-bottom 25%
+      .icon-img
+        position absolute
+        top 0
+        left 0
+        right 0
+        bottom .44rem
+        box-sizing border-box
+        padding .1rem
+        img
+          display block
+          margin 0 auto
+          width 100%
+      .icon-info
+        position absolute
+        left 0
+        right 0
+        bottom 0
+        height .40rem
+        line-height .40rem
+        text-align center
+        color $darkTextColor
+        ellipsis()
 </style>
---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="title">推荐详情</div>
+    <ul>
+      <li class="item">
+        <div class="item-img-wrapper">
+          <img src="" class="item-img" alt="">
+        </div>
+        <div class="item-info">
+          <p class="item-title">最世界榜单：一生必去的50个地方（国内篇）</p>
+          <p class="item-desc">世界那么大，一生是否有足够的时间用脚步丈量世界的每一个角落？</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeRecommend'
+}
+</script>
+<style lang="stylus" scoped>
+  .title
+    marin-top .1rem
+    line-height .8rem
+  .item
+    display flex
+    height 1.9rem
+    overflow hidden
+    .item-img
+      width 1.7rem
+      height 1.7rem
+      padding .1rem
+</style>
