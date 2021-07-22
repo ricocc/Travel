ed362339f417b196285a59cd056e4d71dd363d30
rico <ricocc@qq.com>
2021-07-22 19:02:27 +0800

    article

--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -2,13 +2,13 @@
   <div>
     <div class="title">推荐详情</div>
     <ul>
-      <li class="item">
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
         <div class="item-img-wrapper">
-          <img src="" class="item-img" alt="">
+          <img :src="item.imgUrl" class="item-img" alt="">
         </div>
         <div class="item-info">
-          <p class="item-title">最世界榜单：一生必去的50个地方（国内篇）</p>
-          <p class="item-desc">世界那么大，一生是否有足够的时间用脚步丈量世界的每一个角落？</p>
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
           <button class="item-button">查看详情</button>
         </div>
       </li>
@@ -17,19 +17,87 @@
 </template>
 <script>
 export default {
-  name: 'HomeRecommend'
+  name: 'HomeRecommend',
+  data () {
+   return {
+    recommendList: [{
+        id: '0001',
+        imgUrl: 'https://pic.qyer.com/zhuanlan/article/20210312/864356425875/300x200',
+        title: '最世界榜单：一生必去的50个地方',
+        desc: '世界那么大，一生是否有足够的时间用脚步丈量世界的每一个角落？'
+      }, {
+        id: '0002',
+        imgUrl: 'https://pic.qyer.com/zhuanlan/article/20210312/864356425875/300x200',
+        title: '最世界榜单：一生必去的50个地方',
+        desc: '世界那么大，一生是否有足够的时间用脚步丈量世界的每一个角落？'
+      }, {
+        id: '0003',
+        imgUrl: 'https://pic.qyer.com/zhuanlan/article/20210312/864356425875/300x200',
+        title: '最世界榜单：一生必去的50个地方',
+        desc: '世界那么大，一生是否有足够的时间用脚步丈量世界的每一个角落？'
+      }, {
+        id: '0004',
+        imgUrl: 'https://pic.qyer.com/zhuanlan/article/20210312/864356425875/300x200',
+        title: '最世界榜单：一生必去的50个地方',
+        desc: '世界那么大，一生是否有足够的时间用脚步丈量世界的每一个角落？'
+      }, {
+        id: '0005',
+        imgUrl: 'https://pic.qyer.com/zhuanlan/article/20210312/864356425875/300x200',
+        title: '最世界榜单：一生必去的50个地方',
+        desc: '世界那么大，一生是否有足够的时间用脚步丈量世界的每一个角落？'
+      }, {
+        id: '0006',
+        imgUrl: 'https://pic.qyer.com/zhuanlan/article/20210312/864356425875/300x200',
+        title: '最世界榜单：一生必去的50个地方',
+        desc: '世界那么大，一生是否有足够的时间用脚步丈量世界的每一个角落？'
+      }]
+    }
+  }
 }
 </script>
 <style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
   .title
-    marin-top .1rem
+    margin-top 0.4rem
+    margin-bottom .2rem
     line-height .8rem
+    padding 0 .25rem
+    font-size 0.40rem
+    font-weight bold
   .item
     display flex
     height 1.9rem
     overflow hidden
+    padding .24rem 0.2rem
     .item-img
       width 1.7rem
       height 1.7rem
+      padding .1rem .1rem .1rem 0
+      border-radius 0.2rem
+    .item-info
+      flex 1
       padding .1rem
+      min-width 0
+      .item-title
+        font-size: .32rem
+        font-weight: 700
+        color: rgba(0,0,0,.9)
+        line-height: .48rem
+        margin: 0;
+        max-height: 0.9rem
+        ellipsis()
+      .item-desc
+        font-size: .26rem
+        color: rgba(0,0,0,.60)
+        line-height: .4rem
+        margin-top .2rem
+        ellipsis()
+      .item-button
+        background #ff9300
+        padding 0 .2rem
+        text-align right
+        margin-top .2rem
+        border-radius 0.12rem
+        color #fff
+        line-height .44rem
 </style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,109 @@
+<template>
+  <div>
+    <div class="title">旅程游记</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of WeekendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img :src="item.imgUrl" class="item-img" alt="">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-time">{{item.time}}</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+   return {
+    WeekendList: [{
+        id: '0001',
+        imgUrl: 'http://pic.qyer.com/zhuanlan/article/20210307/982398196989?imageMogr2/auto-orient/thumbnail/!670x300r/gravity/Center/crop/670x300/format/jpg|imageslim',
+        title: '半城烟火半城仙，没有人不会爱上这座“众神之城”！',
+        desc: '清明节三日小长假“嗖”地一下就没了，大部分社畜已经在静候五一劳动节了 ！再上19天的班，上半年最长的小长假就要来了！！...',
+        time: '2月22日, 2021'
+      }, {
+        id: '0002',
+        imgUrl: 'http://pic.qyer.com/zhuanlan/article/20210307/982398196989?imageMogr2/auto-orient/thumbnail/!670x300r/gravity/Center/crop/670x300/format/jpg|imageslim',
+        title: '半城烟火半城仙，没有人不会爱上这座“众神之城”！',
+        desc: '清明节三日小长假“嗖”地一下就没了，大部分社畜已经在静候五一劳动节了 ！再上19天的班，上半年最长的小长假就要来了！！...',
+        time: '2月22日, 2021'
+      }, {
+        id: '0003',
+        imgUrl: 'http://pic.qyer.com/zhuanlan/article/20210307/982398196989?imageMogr2/auto-orient/thumbnail/!670x300r/gravity/Center/crop/670x300/format/jpg|imageslim',
+        title: '半城烟火半城仙，没有人不会爱上这座“众神之城”！',
+        desc: '清明节三日小长假“嗖”地一下就没了，大部分社畜已经在静候五一劳动节了 ！再上19天的班，上半年最长的小长假就要来了！！...',
+        time: '2月22日, 2021'
+      }, {
+        id: '0004',
+        imgUrl: 'http://pic.qyer.com/zhuanlan/article/20210307/982398196989?imageMogr2/auto-orient/thumbnail/!670x300r/gravity/Center/crop/670x300/format/jpg|imageslim',
+        title: '半城烟火半城仙，没有人不会爱上这座“众神之城”！',
+        desc: '清明节三日小长假“嗖”地一下就没了，大部分社畜已经在静候五一劳动节了 ！再上19天的班，上半年最长的小长假就要来了！！...',
+        time: '2月22日, 2021'
+      }, {
+        id: '0005',
+        imgUrl: 'http://pic.qyer.com/zhuanlan/article/20210307/982398196989?imageMogr2/auto-orient/thumbnail/!670x300r/gravity/Center/crop/670x300/format/jpg|imageslim',
+        title: '半城烟火半城仙，没有人不会爱上这座“众神之城”！',
+        desc: '清明节三日小长假“嗖”地一下就没了，大部分社畜已经在静候五一劳动节了 ！再上19天的班，上半年最长的小长假就要来了！！...',
+        time: '2月22日, 2021'
+      }, {
+        id: '0006',
+        imgUrl: 'http://pic.qyer.com/zhuanlan/article/20210307/982398196989?imageMogr2/auto-orient/thumbnail/!670x300r/gravity/Center/crop/670x300/format/jpg|imageslim',
+        title: '半城烟火半城仙，没有人不会爱上这座“众神之城”！',
+        desc: '清明节三日小长假“嗖”地一下就没了，大部分社畜已经在静候五一劳动节了 ！再上19天的班，上半年最长的小长假就要来了！！...',
+        time: '2月22日, 2021'
+      }]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top 0.4rem
+    line-height .8rem
+    padding 0 .25rem
+    font-size 0.40rem
+    font-weight bold
+  .item
+    display flex
+    flex-direction column
+    overflow hidden
+    padding .36rem 0.2rem 0.24rem
+    .item-img-wrapper
+      width 100%
+      .item-img
+        width 100%
+        height auto
+        border-radius 0.2rem
+    .item-info
+      padding 0.24rem 0
+      min-width 0
+      .item-title
+        font-size: .32rem
+        font-weight: 600
+        color: rgba(0,0,0,.9)
+        line-height: .48rem
+        margin: 0;
+        max-height: 0.9rem
+        ellipsis()
+      .item-desc
+        font-size: .26rem
+        color: rgba(0,0,0,.60)
+        line-height: .4rem
+        margin-top .1rem
+        ellipsis()
+      .item-time
+        padding 0 .1rem
+        text-align right
+        margin-top .1rem
+        border-radius 0.12rem
+        color rgba(0,0,0,.40)
+        line-height .24rem
+        font-size 0.2rem
+        background none
+</style>
