c80a0c49f342cd5c9d81585aa24c3e1e3b08e052
ricocc <ricocc@qq.com>
2021-08-19 00:15:12 +0800

    add city css

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -3,18 +3,21 @@
     <city-header></city-header>
     <city-search></city-search>
     <city-list></city-list>
+    <city-alphabet></city-alphabet>
   </div>
 </template>
 <script>
 import CityHeader from './components/Header.vue'
 import CitySearch from './components/Search.vue'
 import CityList from './components/List.vue'
+import CityAlphabet from './components/Alphabet.vue'
 export default {
   name: 'City',
   components: {
     CityHeader,
     CitySearch,
-    CityList
+    CityList,
+    CityAlphabet
   }
 }
 </script>
