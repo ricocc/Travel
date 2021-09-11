1b30a5ca1bc16c3ad26d312fd05bb8808d82ac01
ricocc <ricocc@qq.com>
2021-09-11 19:59:00 +0800

    keep alive

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
   <div id="app">
     <!-- <img src="./assets/logo.png"> -->
     <!-- 显示的是当前路由地址所对应的内容 Hello world 小组件-->
-    <router-view/>
+    <keep-alive>
+      <router-view/>
+    </keep-alive>
   </div>
 </template>
 <script>
