1764c6f4a83115d34acce4e9b549da116c2fce2d
ricocc <ricocc@qq.com>
2021-09-15 00:21:40 +0800

    detail header

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     // allow async-await
     "indent": ["off", 2],
     'generator-star-spacing': 'off',
+    'no-tabs': 'off',
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
